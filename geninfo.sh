429ffdaa597498753013af83dabff4ea9af6497e
Alex Shafer <shafer.alex@gmail.com>
2016-01-11 13:26:06 -0500

    Organize

--- geninfo.sh
+++ geninfo.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-mkdir ~/results/
-mv darbyrecord.br results-columns.txt ~/results/
 git checkout gh-pages
 git pull
-cp ~/results/results-columns.txt ~/results/darbyrecord.br .
+mkdir results
+cp ~/results/* ./results/
 python imagemaker.py > battlegraphic.svg
 git config user.email '@' && git config user.name 'CircleCI'
-git add results-columns.txt darbyrecord.br battlegraphic.svg battlegraphic.png
+git add .
 git commit -m 'Update published battle'
 git push
 git checkout master
-cp ~/results/results-columns.txt ~/results/darbyrecord.br . 
-# so they are still available as artifacts
