dcac2af1b4ef1faf5850265f06cabaef1c86f648
Jacob Sims <jacobsims@users.noreply.github.com>
2016-01-11 21:58:12 -0500

    Update gh-pages in the testing phase to avoid losing results

--- geninfo.sh
+++ geninfo.sh
@@ -6,6 +6,6 @@ mkdir results
 cp -r ~/battles/* .
 git config user.email '@' && git config user.name 'CircleCI'
 git add .
-git commit -m 'Update published battle'
+git commit -m "Update published battles $1"
 git push
 git checkout master
