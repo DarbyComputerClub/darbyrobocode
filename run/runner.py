9eef6f97ad44f0349a2d35b71d4d2df264ce5f5c
Jacob Sims <jacobsims@users.noreply.github.com>
2016-01-12 14:50:42 -0500

    Don't depend on bash, and exit if a battle fails

--- run/runner.py
+++ run/runner.py
@@ -8,9 +8,6 @@ import robosvgmake
 
 battles = ["melee/darby","melee/withsample","1v1/enz_v_jac","1v1/jac_v_wall","1v1/enz_v_wall"]
 
-def bashrun(command):
-    subprocess.call(['bash', '-c', command])
-
 
 run = [battles[i::int(os.environ['CIRCLE_NODE_TOTAL'])] for i in range(int(os.environ['CIRCLE_NODE_TOTAL']))]
 
@@ -25,8 +22,22 @@ print(battleList)
 
 for battle in battleList:
     os.makedirs(os.path.expanduser('~/battles/results/' + battle))
-    bashrun("java -Xmx512M -Dsun.io.useCanonCaches=false -cp libs/robocode.jar robocode.Robocode -battle battles/" + battle + ".battle -nodisplay -results ~/battles/results/" + battle + ".txt -nosound -record ~/battles/results/" + battle + ".br")
-    bashrun("cat <(echo \"Darby Robocode Battle number $CIRCLE_BUILD_NUM (from commit $CIRCLE_SHA1)\") <(column -ts $'\t' ~/battles/results/" + battle + ".txt) > ~/battles/results/" + battle + "-col.txt")
+    exitcode = subprocess.call(['java', '-Xmx512M', '-Dsun.io.useCanonCaches=false', '-cp', 'libs/robocode.jar',
+                                'robocode.Robocode',
+	                        '-battle', 'battles/' + battle + '.battle',
+	                        '-nodisplay',
+	                        '-results', os.path.expanduser('~/battles/results/' + battle + '.txt')
+	                        '-nosound',
+	                        '-record', os.path.expanduser('~/battles/results/' + battle + ".br")])
+    if exitcode != 0:
+        exit(exitcode)
+
+    with open(os.path.expanduser('~/battles/results/' + battle + '-col.txt'), 'w') as w:
+        w.write('Darby Robocode Battle\n')
+        w.write(battle + ' #' + os.environ['CIRCLE_BUILD_NUM'] + '\n')
+	with open(os.path.expanduser('~/battles/results/' + battle + '.txt'), 'r') as r:
+	    w.write(r.read())
+        w.write('\ncommit ' + os.environ['CIRCLE_SHA1'] + '\n')
     
     robosvgmake.create(battle)
 
