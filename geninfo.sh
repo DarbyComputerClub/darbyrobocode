136cfe9fe5ab7e1636f0fd85b112571afbc2ce65
Alex Shafer <shafer.alex@gmail.com>
2016-01-11 20:02:40 -0500

    Fix geninfo

--- geninfo.sh
+++ geninfo.sh
@@ -3,10 +3,7 @@
 git checkout gh-pages
 git pull
 mkdir results
-cp -r ~/battles/* ./results/
-
-
-
+cp -r ~/battles/* .
 git config user.email '@' && git config user.name 'CircleCI'
 git add .
 git commit -m 'Update published battle'
