fbb5f121d2ae1206b0ecf6e4ba213519ba7b7d6e
Jacob Sims <jacobsims@users.noreply.github.com>
2016-01-12 10:46:23 -0500

    More informative commit messages

--- geninfo.sh
+++ geninfo.sh
@@ -6,7 +6,7 @@ mkdir results
 cp -r ~/battles/* .
 git config user.email '@' && git config user.name 'CircleCI'
 git add .
-git commit -m "Update published battles $1"
+git commit -m "Update published battles $1 $CIRCLE_BUILD_NUM"
 git pull
 git push
 git checkout master
