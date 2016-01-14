aebb14eb7bfb5bcc937b8cbfbaf4323c91a41bb6
Alex Shafer <shafer.alex@gmail.com>
2016-01-14 10:31:50 -0500

    Move upload code (2/3) [ci skip]

---
+++ run/upload.py
@@ -0,0 +1,3 @@
+# update the website
+if os.environ['CIRCLE_BRANCH'] == 'master' and os.environ['CIRCLE_PROJECT_USERNAME'] == 'DarbyComputerClub':
+        subprocess.call([os.path.expanduser('~/run/geninfo.sh'), repr(battleList)])
