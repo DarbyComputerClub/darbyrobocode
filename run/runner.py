a2969de836c8884be8f910d485f671572c1ecbb5
Alex Shafer <shafer.alex@gmail.com>
2016-01-14 12:15:02 -0500

    SCP was in wrong order

--- run/runner.py
+++ run/runner.py
@@ -43,4 +43,4 @@ for battle in battleList:
 
 # update the website
 if os.environ['CIRCLE_BRANCH'] == 'master' and os.environ['CIRCLE_PROJECT_USERNAME'] == 'DarbyComputerClub' and not os.environ['CIRCLE_NODE_INDEX'] == '0':
-        subprocess.call(['scp', '-r', 'ubuntu@node0:~/battles/', os.path.expanduser('~/battles/')])
+        subprocess.call(['scp', '-r', os.path.expanduser('~/battles/'), 'ubuntu@node0:~/battles/'])
