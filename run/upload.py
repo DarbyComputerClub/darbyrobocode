0d15d908f0c980961491f731df0229d748bbcedf
Alex Shafer <shafer.alex@gmail.com>
2016-01-14 11:36:43 -0500

    Missing import

--- run/upload.py
+++ run/upload.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import subprocess
 
 # update the website
 if os.environ['CIRCLE_BRANCH'] == 'master' and os.environ['CIRCLE_PROJECT_USERNAME'] == 'DarbyComputerClub' and os.environ['CIRCLE_NODE_INDEX'] == '0':
