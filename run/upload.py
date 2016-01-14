9ce6d9480fb054a94f09299257bfe4c24e82b631
Alex Shafer <shafer.alex@gmail.com>
2016-01-14 11:07:57 -0500

    Correct typos

--- run/upload.py
+++ run/upload.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+
+import os
+
 # update the website
-if os.environ['CIRCLE_BRANCH'] == 'master' and os.environ['CIRCLE_PROJECT_USERNAME'] == 'DarbyComputerClub':
+if os.environ['CIRCLE_BRANCH'] == 'master' and os.environ['CIRCLE_PROJECT_USERNAME'] == 'DarbyComputerClub' and not os.environ['CIRCLE_NODE_INDEX']:
         subprocess.call([os.path.expanduser('~/run/geninfo.sh')])
