08cd0b9a78e8ac8f617e5bb72fce1c8835b8aec8
Alex Shafer <shafer.alex@gmail.com>
2016-01-14 10:29:42 -0500

    Move upload code (1/3) [ci skip]

--- run/runner.py
+++ run/runner.py
@@ -41,7 +41,3 @@ for battle in battleList:
     
     robosvgmake.create(battle)
 
-
-# update the website
-if os.environ['CIRCLE_BRANCH'] == 'master' and os.environ['CIRCLE_PROJECT_USERNAME'] == 'DarbyComputerClub':
-        subprocess.call([os.path.expanduser('~/run/geninfo.sh'), repr(battleList)])
